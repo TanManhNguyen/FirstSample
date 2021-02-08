1ce270c970f634ed73bfa17de4fdf4c4ec310d69
Tan Nguyen <manhtan1509@gmail.com>
2021-02-08 21:45:16 +0100

    Created Sample.cpp

---
+++ sample.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+#define stdi stdin
+#define stdo stdout
+
+#define dbl double 
+#define llo long long 
+
+#define sz size
+#define len length
+#define pb push_back
+#define mp make_pair
+
+#define st first 
+#define nd second 
+#define mid ((l+r)>>1)
+
+#define ft(i,a,b) for (int i=(int)a;i<=(int)b;i++)
+#define fd(i,a,b) for (int i=(int)a;i>=(int)b;i--)
+
+using namespace std;
+const int nmax=4e5+100;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    freopen(".inp","r",stdi);
+    freopen(".out","w",stdo); 
+    int n = 10;
+}
